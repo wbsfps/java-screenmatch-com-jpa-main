dc5610420b181de6bd76f7370485c2ade0361178
William Santana <williamandrade1058@gmail.com>
2024-06-29 12:39:24 -0300

    filtrando pela data de lancamento

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -39,6 +39,7 @@ public class Principal {
                     8 - Desafio
                     9 - Buscar episódios por trecho
                     10 - Listando top 5 episódios da série
+                    11 - Filtrando episódios por data
                     0 - Sair
                     """;
 
@@ -75,6 +76,9 @@ public class Principal {
                     break;
                 case 10:
                     topEpisodiosPorSerie();
+                    break;
+                case 11:
+                    buscandoAPartirDeUmaData();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -195,6 +199,16 @@ public class Principal {
                    System.out.printf("Série: %s Temporada %s - Episódio %s - %s - Avaliação: %s\n",
                            e.getSerie().getTitulo(), e.getTemporada(),
                            e.getNumeroEpisodio(), e.getTitulo(), e.getAvaliacao()));
+        }
+    }
+    private void buscandoAPartirDeUmaData(){
+        buscarSeriePorTitulo();
+        if (serieBusca.isPresent()) {
+            Serie serie = serieBusca.get();
+            System.out.println("Informe a data que deseja: ");
+            var anoLancamento = leitura.nextInt();
+            List<Episodio> episodiosAno = repository.episodiosPorSerieEAno(serie, anoLancamento);
+            episodiosAno.forEach(System.out::println);
         }
     }
 }
